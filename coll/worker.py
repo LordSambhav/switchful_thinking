56dc81ea36fe570b6eeb4f06645ba28333b68b31
lordsambhav <bhurtel.sambhav20@gmail.com>
2026-07-14 20:03:30 +0200

    empirical worker implementation - yet to test

--- coll/worker.py
+++ coll/worker.py
@@ -1,15 +1,38 @@
 import argparse
 from util.collectives import Collectives, Test
 from util.network import get_ip, set_drop_prob, recv, send
+import socket, struct
+
+COLL_PORT = 55555
+BROADCAST_IP = "10.0.0.255"
 
 class MyCollectives(Collectives):
     def __init__(self, rank, world):
         self.rank = rank
         self.world = world
+        self.sock = socket.socket(socket.AF_NET, socket.SOCK_DGRAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((get_ip(), COLL_PORT))
 
     def AllReduce(self, input: list[int], output: list[int], op : str = "sum"):
         assert len(input), "input cannot be empty"
         assert len(input) == len(output), "input and output must have the same size"
+        for index, value in enumerate(input):
+            # This one is for reference. the p4 switch accepts the collect_t header in this format. We should construct a packet in the same manner
+            # header collect_t {
+            #     bit<32> index;
+            #     int<32> value;
+            #     }
+            pkt = struct.pack(">Ii", index, value) #here >Ii creates 32+32 bits of index and value making it equal to the header in p4 switch
+            #now we use the send method as suggested in the lab requirement to send the packet
+            send(self.sock, pkt, (BROADCAST_IP, COLL_PORT))
+            data_byte, add = recv(self.sock, 1024)
+
+            #unpack the received data byte now
+            idx, result = struct.unpack(">Ii", data_byte[:8]) #the response is the same 32 bit index + 32 bit value (updated after reduce) = 8 bytes, Ii is used again to unpack the same way as packed
+            output[index] = result #store the result in worker's output array vector
+
         # TODO: Implement me. Ignore the op argument unless you are attempting the bonus
 
     def ReduceScatter(self, input: list[int], output: list[int]):
